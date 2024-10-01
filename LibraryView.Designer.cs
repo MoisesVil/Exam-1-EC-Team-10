5655156bb6a07dc21052ffc9df3489714d798fe3
ksuslinker <slinker@ksu.edu>
2024-10-01 13:50:00 -0500

    gui for libraryview done

--- LibraryView.Designer.cs
+++ LibraryView.Designer.cs
@@ -28,20 +28,124 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.addToLibBtn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listCloudLib = new System.Windows.Forms.ListBox();
+            this.viewABookBtn = new System.Windows.Forms.Button();
+            this.listBookLib = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // addToLibBtn
+            // 
+            this.addToLibBtn.BackColor = System.Drawing.Color.NavajoWhite;
+            this.addToLibBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.addToLibBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.1F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.addToLibBtn.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.addToLibBtn.Location = new System.Drawing.Point(316, 358);
+            this.addToLibBtn.Margin = new System.Windows.Forms.Padding(1);
+            this.addToLibBtn.Name = "addToLibBtn";
+            this.addToLibBtn.Size = new System.Drawing.Size(186, 38);
+            this.addToLibBtn.TabIndex = 1;
+            this.addToLibBtn.Text = "Add To Library";
+            this.addToLibBtn.UseVisualStyleBackColor = false;
+            this.addToLibBtn.Click += new System.EventHandler(this.addToLibBtnClick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Rockwell Extra Bold", 20.1F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(78, 17);
+            this.label1.Margin = new System.Windows.Forms.Padding(1, 0, 1, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(394, 32);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Welcome to MyReader+";
+            // 
+            // listCloudLib
+            // 
+            this.listCloudLib.FormattingEnabled = true;
+            this.listCloudLib.Location = new System.Drawing.Point(316, 92);
+            this.listCloudLib.Margin = new System.Windows.Forms.Padding(1);
+            this.listCloudLib.Name = "listCloudLib";
+            this.listCloudLib.Size = new System.Drawing.Size(188, 251);
+            this.listCloudLib.TabIndex = 3;
+            // 
+            // viewABookBtn
+            // 
+            this.viewABookBtn.BackColor = System.Drawing.Color.NavajoWhite;
+            this.viewABookBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.viewABookBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.1F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.viewABookBtn.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.viewABookBtn.Location = new System.Drawing.Point(25, 358);
+            this.viewABookBtn.Margin = new System.Windows.Forms.Padding(1);
+            this.viewABookBtn.Name = "viewABookBtn";
+            this.viewABookBtn.Size = new System.Drawing.Size(188, 38);
+            this.viewABookBtn.TabIndex = 4;
+            this.viewABookBtn.Text = "View Book";
+            this.viewABookBtn.UseVisualStyleBackColor = false;
+            this.viewABookBtn.Click += new System.EventHandler(this.viewBookBtn);
+            // 
+            // listBookLib
+            // 
+            this.listBookLib.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.listBookLib.FormattingEnabled = true;
+            this.listBookLib.Location = new System.Drawing.Point(25, 92);
+            this.listBookLib.Margin = new System.Windows.Forms.Padding(1);
+            this.listBookLib.Name = "listBookLib";
+            this.listBookLib.Size = new System.Drawing.Size(188, 251);
+            this.listBookLib.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(361, 71);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(101, 20);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Cloud Library";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(90, 71);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(56, 20);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Library";
             // 
             // LibraryView
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.SaddleBrown;
+            this.ClientSize = new System.Drawing.Size(529, 406);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.listBookLib);
+            this.Controls.Add(this.viewABookBtn);
+            this.Controls.Add(this.listCloudLib);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.addToLibBtn);
+            this.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
             this.Name = "LibraryView";
             this.Text = "Library View";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+        private System.Windows.Forms.Button addToLibBtn;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox listCloudLib;
+        private System.Windows.Forms.Button viewABookBtn;
+        private System.Windows.Forms.ListBox listBookLib;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
     }
 }
 
--- LibraryView.cs
+++ LibraryView.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +16,30 @@ namespace Exam_1_EC
         public LibraryView()
         {
             InitializeComponent();
+            fillCloudLibOnOpen();
+        }
+
+        private SyncLibrary syncLibHan;
+        private LibraryModel libraryModel;
+       
+        private void listCloudLibSelectedBook(object sender, EventArgs e)
+        {
+
+        }
+
+        private void addToLibBtnClick(object sender, EventArgs e)
+        {
+            //syncLibHan(listCloudLib.SelectedValue);
+        }
+        public void fillCloudLibOnOpen()
+        {
+            //syncLibHan();
+        }
+
+        private void viewBookBtn(object sender, EventArgs e)
+        {
+            BookView bV = new BookView();
+            bV.Show();
         }
     }
 }
